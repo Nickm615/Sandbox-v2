81aa1af615ced3f8553ce246b98a20ea57bed1f6
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2022-10-14 12:41:44 -0500

    Rich Text Resolver in place for body text, other resolvers not complete

--- src/components/_product.tsx
+++ src/components/_product.tsx
@@ -1,12 +1,14 @@
 import { parse } from 'node-html-parser'
 import { ExtendedProduct, Product } from "../models"
 import {FunctionComponent} from 'react';
+import { RichText } from './richText'
 
 type ProductProps = ExtendedProduct
 export const ProductComponent = ({elements, shopifyObject}: ProductProps)=> 
         <div className="product-card">
             <img className="product-img" width={'300px'} height={'200px'} src={shopifyObject.previewUrl}/>
-            <p>{shopifyObject.title}</p>
+            <h4 className='product-description'>{shopifyObject.title}</h4>
+            <RichText element={elements.description}></RichText>
             <div>
             </div>
         </div>
