b36877da0224f17486ab586ee674f2f1cd8614a4
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2022-12-06 14:01:01 -0600

    Filled out home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { ProductListing } from './pages/ProductListing';
+import { Translation } from './pages/Translation'
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/products" element={<ProductListing/>} />
+          <Route path='/' element={<Home/>} />
+          <Route path='/products' element={<ProductListing/>} />
+          <Route path='/translation' element={<Translation/>}/>
         </Routes>
 
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
-import UnderConstruction from '../media/const.jpg'
+import { Link } from 'react-router-dom'
 
 
 export function Home() {
     return(
-        <div>
+        <div className='home-page'>
             <h1>Home Page</h1>
-            <img src={UnderConstruction}></img>
+            <h2>Welcome to the support sandbox! The purpose of this space is to host examples of various integrations for the purposes of testing by the support team. If you need access to this or would like to contribute, please contact me at nicholasm@kontent.ai </h2>
+            <Link to='/products'>Algolia Product Search</Link>
         </div>
 
     )
